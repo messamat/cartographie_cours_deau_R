d4ce8132ef8feb1812a43518d7b1b471c06aac8c
messamat <messamat@uw.edu>
2023-11-07 19:26:35 -0500

    Set-up R project

---
+++ _targets.R
@@ -0,0 +1,12 @@
+source("R/cartoCE_packages.R")
+source("R/cartoCE_functions.R")
+
+rootdir = getwd() #rprojroot::find_root(has_dir('R'))
+datdir = file.path(rootdir, 'data')
+resdir = file.path(rootdir, 'results')
+
+#--- Data repositories --------------------------------------------------------
+
+
+#--- Define targets plan ------------------------------------------------------
+
