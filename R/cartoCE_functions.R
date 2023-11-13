96868cbf45d626f84ec2bc07f1eb26d83f9bd91a
messamat <messamat@uw.edu>
2023-11-13 16:46:24 -0500

    Import pre-processed data, format metadata about DDT networks, start Quarto report

--- R/cartoCE_functions.R
+++ R/cartoCE_functions.R
@@ -1,1 +1,90 @@
 #FUNCTIONS
+
+#in_metadata_nets <- tar_read(metadata_nets)
+format_metadata_nets <- function(in_metadata_nets) {
+  
+  #Rename columns
+  cols_rename <- list(
+    c("Numéro", 'dep_code'),
+    c("Département", 'dep_name'),
+    c("Nombre total d'écoulements référencés", "nlines_reported"),
+    c("Nom d'attributs", "attri_names"),
+    c("Nom de l'attribut désignant le type d'écoulement", "type_stand_name"),
+    c("Catégories de l'attribut désignant le type d'écoulement", "type_stand_cats"),
+    c("CE_recatégorisé", "ce_recat"),
+    c("NCE_recatégorisé", "nce_recat"),
+    c("Indéterminé_ou_autre_recatégorisé", "ind_recat"),
+    c("Inexistant_recatégorisé", "inx_recat"),
+    c("Hors_département_recatégorisé", "hd_recat"),
+    c("Nom de l'attribut auxiliaire désignant le type d'écoulement", "type_aux_name"),
+    c("Nom de l'attribut désignant le régime hydrologique", "regime_name"),
+    c("Nom de l'attribut désignant la méthode d'identification de l'écoulement", "nat_id_name"),
+    c("Nom de l'attribut désignant la source de la modification; de la suppression du tronçon BD TOPO®; ou de l’ajout d’un nouveau tronçon", "orig_mo_name"),
+    c("Nom de l'attribut désignant la date de l'identification du type d'écoulement", "date_id_name"),
+    c("Origine des données", "data_orig"),
+    c("Commentaire", "com"),
+    c("Date de revision affichée (XML)", "date_rev"),
+    c("Date de publication affichée (XML)", "date_pub"),
+    c("Date d'obtention", "date_get"),
+    c("Inclusion des non cours d'eau", "nce_included"),
+    c("Lien local données", "local_url")
+  ) %>%
+    as.data.frame %>%
+    t %>%
+    as.data.table %>%
+    setnames(c('old_col', 'new_col'))
+  
+  #Remove superfluous columns
+  mdat_dt <- as.data.table(in_metadata_nets) %>%
+    .[, -c('Titre de la couche selon XML', 'Titre du fichier','Statut (XML)', 
+           'Abstract (XML)', 'Mise a jour (XML)', 'Format', 'Encodage',
+           'Echelle (XML)', 'Lien URL', 'Lineage (XML)', 'Projection',
+           "Explication des catégories de l'attribut désignant le statut du cours d'eau si besoin")] %>%
+    setnames(cols_rename$old_col, cols_rename$new_col)
+  
+  #Compile instances with multiple layers (3 departments)
+  mdat_dt_comb1 <- mdat_dt[
+    ,.(nlines_reported = sum(nlines_reported),
+       nce_included = data.table::last(sort(nce_included)), #Keep yes if there is a Yes (O) and No (N)
+       date_rev= data.table::first(date_rev),
+       date_pub= data.table::first(date_pub),
+       date_get= data.table::first(date_get)
+    ),
+    by=c('dep_code', 'dep_name')]
+  
+  collapse_cols <- c("attri_names", "type_stand_name", "type_stand_cats",
+                     "ce_recat","nce_recat", "ind_recat", "inx_recat", 
+                     "hd_recat", "nce_included", "type_aux_name", "regime_name",
+                     "nat_id_name", "orig_mo_name", "date_id_name", "date_rev",
+                     "date_pub", "data_orig", "date_get", "local_url", "com")
+  
+  mdat_dt_comb <- 
+    lapply(collapse_cols, function(col_name) {
+      mdat_dt[
+        , paste(get(col_name), collapse=';')
+        , by='dep_code'][, 'V1', with=F] %>%
+        setnames(col_name)
+    }) %>% do.call(cbind, .) %>%
+    cbind(mdat_dt_comb1, .)
+  
+  mdat_dt_uform <-  mdat_dt_comb[
+    dep_code %in% mdat_dt[duplicated(mdat_dt$dep_code), dep_code],
+    (collapse_cols) := sapply(.SD, function(string) {
+      strsplit(string, ';')[[1]] %>%
+        unique %>%
+        .[!(. %in% "Pas de catégorie correspondante")] %>%
+        paste(collapse=";")
+    },
+    simplify=FALSE),
+    .SDcols = collapse_cols,
+    by='dep_code']
+  
+  
+  return( mdat_dt_uform)
+}
+
+# tar_load(bcae_bvinters)
+# tar_load(bdtopo_bvinters)
+# tar_load(rht_bvinters)
+# tar_load(carthage_bvinters)
+# tar_load(ddtnets_bvinters)
--- _targets.R
+++ _targets.R
@@ -1,12 +1,97 @@
 source("R/cartoCE_packages.R")
 source("R/cartoCE_functions.R")
 
-rootdir = getwd() #rprojroot::find_root(has_dir('R'))
+rootdir = rprojroot::find_root(has_dir('src'))
 datdir = file.path(rootdir, 'data')
 resdir = file.path(rootdir, 'results')
 
+tar_option_set(format = "qs")
+
 #--- Data repositories --------------------------------------------------------
+statsgdb = file.path(resdir, 'env_stats.gdb')
+list.files(resdir)
+
+############################# Define targets plan ##############################
+list(
+  #------------------------------- Read files ----------------------------------
+  #Read in metadata
+  tar_target(ddt_metadata_path,
+             file.path(datdir, 
+                       'metadonnes_cartographie_cours_deau_20231106.xlsx')
+             ) #format='file'
+    ,
+  
+  #Establish csv file paths (to be reactive to file updates)
+  tar_target(bcae_bvinters_path, file.path(resdir, "bcae_fr_bvinters.csv"),format = 'file'), #BCAE
+  tar_target(bdtopo_bvinters_path, file.path(resdir, "bdtopo2015_fr_bvinters.csv"),format = 'file'), #BDTOPO
+  tar_target(rht_bvinters_path, file.path(resdir, "rht_lbt93_bvinters.csv"),format = 'file'), #RHT
+  tar_target(carthage_bvinters_path, file.path(resdir, "TRONCON_HYDROGRAPHIQUE_bvinters.csv"  ),format = 'file'), #Carthage
+  tar_target(ddtnets_bvinters_path, file.path(resdir, "carto_loi_eau_fr_bvinters.csv"),format = 'file'), #DDT harmonized networks
+  
+  tar_target(amber_bvinters_path, file.path(resdir, "barriers_amber_bvinters.csv"),format = 'file'), #barriers
+  tar_target(bdforest_bvinters_path, file.path(resdir, "bdforet_fr_bvinters.csv"),format = 'file'), #forest type
+  tar_target(bdhaies_bvinters_path, file.path(resdir, "bdhaies_bvinters.csv"),format = 'file'), #hedges
+  tar_target(comirrig_bvinters_path, file.path(resdir, "communes_irrig_bvinters.csv"),format = 'file'), #commune-based irrigation
+  tar_target(bdcharm_bvinters_path, file.path(resdir, "lithology_bdcharm_fr_bvinters.csv"),format = 'file'), #lithology
+  tar_target(onde_stations_bvinters_path, file.path(resdir, "onde_stations_bvinters.csv"),format = 'file'), #stations of intermittency observation
+  tar_target(snelder_bvinters_path, file.path(resdir, "snelder_ires_bvinters.csv"),format = 'file'),
+  tar_target(bnpe_bvinters_path, file.path(resdir, "withdrawals_bnpe_proj_bvinters.csv"),format = 'file'), #withdrawals
+  
+  #Read in DDT metadata
+  tar_target(metadata_sources, read_xlsx(path = ddt_metadata_path, sheet="Sources")),
+  tar_target(metadata_nets, read_xlsx(path = ddt_metadata_path, sheet="Métadonnées_réseau_SIG")),
+  tar_target(metadata_websites, read_xlsx(path = ddt_metadata_path, sheet="Données_sites_DDT")),
+  
+  #Read in csvs of networks
+  tar_target(bcae_bvinters, fread(bcae_bvinters_path)), #BCAE
+  tar_target(bdtopo_bvinters, fread(bdtopo_bvinters_path)), #BDTOPO
+  tar_target(rht_bvinters, fread(rht_bvinters_path)), #RHT
+  tar_target(carthage_bvinters, fread(carthage_bvinters_path)), #Carthage
+  tar_target(ddtnets_bvinters, fread(ddtnets_bvinters_path)), #DDT harmonized networks
+  
+  #Read in csvs of environmental files
+  # tar_target(amber_bvinters, fread(amber_bvinters_path)), #barriers
+  # tar_target(bdforest_bvinters, fread(bdforest_bvinters_path)), #forest type
+  # tar_target(bdhaies_bvinters, fread(bdhaies_bvinters_path)), #hedges
+  # tar_target(comirrig_bvinters, fread(comirrig_bvinters_path)), #commune-based irrigation
+  # tar_target(bdcharm_bvinters, fread(bdcharm_bvinters_path)), #lithology
+  # tar_target(onde_stations_bvinters, fread(onde_stations_bvinters_path)), #stations of intermittency observation
+  # tar_target(snelder_bvinters, fread(snelder_bvinters_path)),
+  # tar_target(bnpe_bvinters, fread(bnpe_bvinters_path)), #withdrawals
+  
+  #Read and merge gdb tables
+  tar_target(
+    env_gdbtabs,
+    lapply(c('ari_ix_ssu','ari_ix_syr'
+             ,'awc_mm_sav0_5' ,'awc_mm_sav5_15' ,'awc_mm_sav15_30'
+             ,'awc_mm_sav30_60','awc_mm_sav60_100','awc_mm_sav100_200'
+             ,'cly_pc_sav0_5','cly_pc_sav5_15','cly_pc_sav15_30'
+             ,'cly_pc_sav30_60','cly_pc_sav60_100','cly_pc_sav100_200'
+             ,'slt_pc_sav0_5','slt_pc_sav5_15','slt_pc_sav15_30'
+             ,'slt_pc_sav30_60','slt_pc_sav60_100','slt_pc_sav100_200'
+             ,'snd_pc_sav0_5','snd_pc_sav5_15','snd_pc_sav15_30'
+             ,'snd_pc_sav30_60','snd_pc_sav60_100','snd_pc_sav100_200'
+             ,'lc_pc_s19','lc_pc_s20','lc_pc_s21','slo_dg_sav'),
+           function(lyr) {
+             as.data.table(st_read(statsgdb, layer=lyr)) %>%
+               setnames(
+                 old = names(.)[-(names(.)=='UID_BV')],
+                 new = paste(names(.)[-(names(.)=='UID_BV')],lyr,sep='_')
+               )
+           }) %>% 
+      Reduce(function(x, y) merge(x, y, by="UID_BV"), .)
+  )
+  ,
+  tar_target(
+    metadata_nets_formatted,
+    format_metadata_nets(metadata_nets)
+  )
+  
+  #------------------------------- Format statistics ---------------------------
+  #amber_bvinters[, .N, by=c('LabelAtlas', 'UID_BV')]
+
+)
 
 
-#--- Define targets plan ------------------------------------------------------
+
 
